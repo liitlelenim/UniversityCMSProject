6134a10666e1e3303f8ca963ebae9b977f9fddca
Jakub Błaszczyk <72347694+liitlelenim@users.noreply.github.com>
2026-01-18 16:10:30 +0100

    Add user page and page tag model

---
+++ Models/PageTag.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityCMSProject.Models;
+
+public class PageTag
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Tag name is required")]
+    [StringLength(30, ErrorMessage = "Tag name must be less than 30 characters")]
+    [RegularExpression(@"^[a-zA-Z0-9\-]+$",
+        ErrorMessage = "Tag can only contain letters, numbers and hyphens")]
+    public string Name { get; set; }
+    
+    public ICollection<UserPage> Pages { get; set; } = new List<UserPage>();
+}
---
+++ Models/UserPage.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityCMSProject.Models;
+
+public class UserPage
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Title is required")]
+    [StringLength(100)]
+    public string Title { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    [RegularExpression(@"^[a-z0-9\-]+$")]
+    public string Url { get; set; }
+
+    [Required]
+    [MinLength(10)]
+    public string HtmlContent { get; set; }
+
+    public string CssContent { get; set; }
+    public string JsContent { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public bool IsPublic { get; set; } = true;
+
+    public string UserId { get; set; }
+    public AppUser User { get; set; }
+
+    public List<PageTag> Tags { get; set; } = new List<PageTag>();
+}
