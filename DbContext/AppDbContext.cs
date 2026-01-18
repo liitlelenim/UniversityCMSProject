9c0879584fdcfa4ca8374c9c26f2668561b36e5c
Jakub Błaszczyk <72347694+liitlelenim@users.noreply.github.com>
2026-01-18 16:10:48 +0100

    Update app db context for handling of added models

---
+++ DbContext/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UniversityCMSProject.Models;
+
+namespace UniversityCMSProject;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<AppUser>(options)
+{
+    public DbSet<UserPage> UserPages { get; set; }
+    public DbSet<PageTag> PageTags { get; set; }
+}
